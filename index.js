06a55503786817701317a68910b8234d89ed956c
Zyrkos <pedrocastro1995@hotmail.com>
2023-03-18 16:35:44 +0000

    .

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const pagesInput = document.getElementById("pages");
 const readInput = document.getElementById("read");
 const bookContainer = document.getElementById("book-container");
 
-function Book(title, author, pages, isRead) {
+/* function Book(title, author, pages, isRead) {
   constructor(
     title = 'Unknown';
     author = 'Unknown';
@@ -18,21 +18,43 @@ function Book(title, author, pages, isRead) {
   this.pages = pages;
   this.isRead = isRead;
 }
-}
+} */
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const title = titleInput.value;
+  const author = authorInput.value;
+  const pages = pagesInput.value;
+  const read = readInput.value;
+
+  
+  const newBookCard = document.createElement("div");
+  newBookCard.classList.add("book-card");
+  newBookCard.innerHTML = `
+    <h2 class="book-title">${title}</h2>
+    <p class="book-author">${author}</p>
+    <p class="book-pages">${pages}</p>
+    <p class="book-read">${read}</p>
+  `;
+  
+  bookContainer.appendChild(newBookCard);
+
+  // clear the input fields after submitting
+  titleInput.value = "";
+  authorInput.value = "";
+  pagesInput.value = "";
+  readInput.value = "";
 
 
+})
 
-function addBookToLibrary(title, author, pages, isRead) {
+/* function addBookToLibrary(title, author, pages, isRead) {
   let newBook = new Book(title, author, pages, isRead);
   myLibrary.push(newBook);
   
 
   
-}
-addBookToLibrary("Harry Potter", "JK Rowling", "600", "read");
+} */
 
-addBookToLibrary("LOTR", "Tolkien", "200", "read");
-
-console.log(myLibrary)
 
 
