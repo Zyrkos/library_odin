650c65ec82aff0bca978700ee6a90648b2c923e2
Zyrkos <pedrocastro1995@hotmail.com>
2023-03-18 16:15:12 +0000

    .

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 const myLibrary = [];
+const form = document.getElementById("book-form");
+const titleInput = document.getElementById("title");
+const authorInput = document.getElementById("author");
+const pagesInput = document.getElementById("pages");
+const readInput = document.getElementById("read");
+const bookContainer = document.getElementById("book-container");
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages, isRead) {
+  constructor(
+    title = 'Unknown';
+    author = 'Unknown';
+    pages = '0';
+    isRead = false;
+  ) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.read = read;
+  this.isRead = isRead;
+}
 }
 
 
 
-function addBookToLibrary(title, author, pages, read) {
-  let newBook = new Book(title, author, pages, read);
+function addBookToLibrary(title, author, pages, isRead) {
+  let newBook = new Book(title, author, pages, isRead);
   myLibrary.push(newBook);
   
 
