76f41b7a696ace03060bd2edf9e2451c50043d88
Pedro Castro <106441980+Zyrkos@users.noreply.github.com>
2023-03-18 14:06:42 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const myLibrary = [];
 
-
-
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.read = read 
+  this.read = read;
 }
 
-function addBookToLibrary() {
+
+
+function addBookToLibrary(title, author, pages, read) {
+  let newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+  
+
   
 }
+addBookToLibrary("Harry Potter", "JK Rowling", "600", "read");
+
+addBookToLibrary("LOTR", "Tolkien", "200", "read");
+
+console.log(myLibrary)
 
 
